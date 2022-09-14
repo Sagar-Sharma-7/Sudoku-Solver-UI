badd583bd3bdc2b32fee2e4ef9f731ab3ee6126d
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-09-14 20:57:49 +0530

    found filling bug

--- public/script/main.js
+++ public/script/main.js
@@ -3,24 +3,38 @@ const num = document.querySelectorAll(".num");
 const solve_btn = document.querySelector("#solve_btn");
 
 
-boxes.forEach(box => {
-    box.addEventListener("click", () => {
-        num.forEach(item => {
-            item.addEventListener("click", () =>{
-                let n = item.innerHTML;
-                box.innerHTML = n;
-            });
-        });
-    });
-});
 
+let i = 0;
+let f = true;
+const filling = () => {
+    boxes.forEach(b => {
+        b.style.borderColor = "rgb(125, 125, 125)";
+        b.style.borderWidth = "1px";
+    })
+    boxes[i].style.borderColor = "#ACFBAF";
+    boxes[i].style.borderWidth = "2px"
+       num[1].onclick = () => {
+            boxes[i].innerHTML = 1;
+            console.log(i)
+            i += 1;
+            if(i < 81){
+                f = true;
+                filling();
+            }else{
+                f = false;
+            }
+    };
+}
 
+if(f == true){
+    filling();
+}
 
-solve_btn.addEventListener("click", () => {
-    boxes.forEach(box => {
-        if(box.innerHTML == ""){
-                box.innerHTML = 0;
-        };
-    });
+// solve_btn.addEventListener("click", () => {
+//     boxes.forEach(box => {
+//         if(box.innerHTML == ""){
+//                 box.innerHTML = 0;
+//         };
+//     });
 
-});
+// });
