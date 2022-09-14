609c9dd2225ed5920ccfd74519c6edc73d6e41a9
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-09-14 21:14:41 +0530

    filling bug solved

--- public/script/main.js
+++ public/script/main.js
@@ -3,7 +3,6 @@ const num = document.querySelectorAll(".num");
 const solve_btn = document.querySelector("#solve_btn");
 
 
-
 let i = 0;
 let f = true;
 const filling = () => {
@@ -11,19 +10,30 @@ const filling = () => {
         b.style.borderColor = "rgb(125, 125, 125)";
         b.style.borderWidth = "1px";
     })
-    boxes[i].style.borderColor = "#ACFBAF";
-    boxes[i].style.borderWidth = "2px"
-       num[1].onclick = () => {
-            boxes[i].innerHTML = 1;
+    if( i <= 80){
+        boxes[i].style.borderColor = "#ACFBAF";
+        boxes[i].style.borderWidth = "2px";
+    }
+    const fill = (x) => {
+        if(i <= 80){
+        boxes[i].innerHTML = x;
             console.log(i)
             i += 1;
-            if(i < 81){
-                f = true;
                 filling();
-            }else{
-                f = false;
-            }
-    };
+        }else{
+            f = false;
+        }
+    }
+    num[0].onclick = () => {fill(0)};
+    num[1].onclick = () => {fill(1)};
+    num[2].onclick = () => {fill(2)};
+    num[3].onclick = () => {fill(3)};
+    num[4].onclick = () => {fill(4)};
+    num[5].onclick = () => {fill(5)};
+    num[6].onclick = () => {fill(6)};
+    num[7].onclick = () => {fill(7)};
+    num[8].onclick = () => {fill(8)};
+    num[9].onclick = () => {fill(9)};
 }
 
 if(f == true){
